4b32cdea8cdb480511cbd10cdafd0aac96c03492
John McPherson <john@jrmcpherson.com>
2011-06-18 19:03:54 -0500

    added initial files

---
+++ app.rb
@@ -0,0 +1,58 @@
+require 'sinatra/base'
+require 'resque'
+require 'job'
+
+module Demo
+  class App < Sinatra::Base
+    get '/' do
+      info = Resque.info
+      out = "<html><head><title>Resque Demo</title></head><body>"
+      out << "<p>"
+      out << "There are #{info[:pending]} pending and "
+      out << "#{info[:processed]} processed jobs across #{info[:queues]} queues."
+      out << "</p>"
+      out << '<form method="POST">'
+      out << '<input type="submit" value="Create New Job"/>'
+      out << '&nbsp;&nbsp;<a href="/resque/">View Resque</a>'
+      out << '</form>'
+      
+       out << "<form action='/failing' method='POST''>"
+       out << '<input type="submit" value="Create Failing New Job"/>'
+       out << '&nbsp;&nbsp;<a href="/resque/">View Resque</a>'
+       out << '</form>'
+      
+      out << "</body></html>"
+      out
+    end
+
+    post '/' do
+      Resque.enqueue(Job, params)
+      redirect "/"
+    end
+    
+    post '/failing' do
+      Resque.enqueue(FailingJob, params)
+      redirect "/"
+    end
+  end
+end
+
+
+
+Dedicated Server Powered by the Dedicated Servers and
+Cloud Computing of Rackspace Hosting®
+
+    Blog
+    About
+    Contact & Support
+    Training
+    Job Board
+    Shop
+    API
+    Status
+
+    © 2011 GitHub Inc. All rights reserved.
+    Terms of Service
+    Privacy
+    Security
+
